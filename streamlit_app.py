5118ad7eb57a7f1537fc649dfbe6ee7e8a2bfe65
chitemerere <chitemerere.stratdigm@gmail.com>
2024-01-23 09:30:47 +0200

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# streamlit_app.py
+
+import streamlit as st
+
+# Initialize connection.
+conn = st.connection('mysql', type='sql')
+
+# Perform query.
+df = conn.query('SELECT * from mytable;', ttl=600)
+
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.name} has a :{row.pet}:")
+
